87db2e6585e53d1cd1cfb0a5f31c1bca27895639
Samir <samirtula@mail.ru>
2020-08-29 17:05:16 +0300

    shefsclubjs

---
+++ shefs_club+js/script.js
@@ -0,0 +1,8 @@
+$(".switch-btn").click(function () {
+  $(this).toggleClass("switch-on");
+  if ($(this).hasClass("switch-on")) {
+    $(this).trigger("on.switch");
+  } else {
+    $(this).trigger("off.switch");
+  }
+});
