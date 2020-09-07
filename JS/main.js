416adef728ba962099db584fa380c4a3931885aa
Samir Gakhramanov <samirtula@mail.ru>
2020-09-07 21:27:07 +0300

    JS

---
+++ JS/main.js
@@ -0,0 +1,37 @@
+/*alert("Привет");
+console.log("привет в консоле");
+let name = "Ivan",
+sername = 'Shishkin';
+
+let myName = yourName = "Ivan"; // две в одной
+console.log(yourName);
+yourName = "Sveta";
+console.log(yourName)
+
+var - устаревшая форма.
+
+const myName = "Ivan"// Константа - не изменяемая форма
+переменные чувствительны к регистру
+
+наименование начинать не с цифры, не с символа
+
+camelcase в вычляемой переменной
+capslock в константах*/
+
+
+
+let admin;
+let name;
+name = "Jhon";
+admin = name;
+alert(admin);
+
+
+let myPlanetName;
+let profileName;
+
+
+
+верхний регистр
+для birthday - можно
+для age - нет т.к это вычисляемая переменная
