0272a35481c465001bd017309201169eda249082
Samir Gakhramanov <samirtula@mail.ru>
2020-11-09 21:18:17 +0300

    JS

---
+++ Learning_JS/Jquery 1/app.js
@@ -0,0 +1,9 @@
+$(document).ready(function (e) {
+  $("#pmtog").css("color", "green");
+  $("p.pr1").css("color", "red");
+  $("[type='button']").css("color", "orange");
+  $("p.pmb1, .pmb2, .pmb3").css("color", "blue");
+  $("#wrap1 > p:nth-child(2)").css("color", "brown");
+  $("[src='mountimg2.jpg']").css("borderStyle", "solid");
+  $("#iwrap > input:nth-child(odd)").css("color", "red");
+});
---
+++ Learning_JS/Jquery 2/app.js
@@ -0,0 +1,16 @@
+/* $(document).ready(function (e) {
+  $("#pmtog").css("color", "green");
+  $("p.pr1").css("color", "red");
+  $("[type='button']").css("color", "orange");
+  $("p.pmb1, .pmb2, .pmb3").css("color", "blue");
+  $("#wrap1 > p:nth-child(2)").css("color", "brown");
+  $("[src='mountimg2.jpg']").css("borderStyle", "solid");
+  $("#iwrap > input:nth-child(odd)").css("color", "red");
+});
+ */
+
+$(document).ready(function (e) {
+  $("#but1").click(function () {
+    $("#par1").css("{color", "green}");
+  });
+});
