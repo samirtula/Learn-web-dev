0fad700fcc9916ca2a0175809b9e1c154e256f16
Samir Gakhramanov <samirtula@mail.ru>
2020-11-16 21:20:43 +0300

    JS

---
+++ Learning_JS/Cookie/app.js
@@ -0,0 +1,51 @@
+function getCookie(name) {
+  let matches = document.cookie.match(
+    new RegExp(
+      "(?:^|; )" +
+        name.replace(/([\.$?*|{}\(\)\[\]\\\/\+^])/g, "\\$1") +
+        "=([^;]*)"
+    )
+  );
+  return matches ? decodeURIComponent(matches[1]) : undefined;
+}
+function setCookie(name, value, options = {}) {
+  options = {
+    path: "/",
+    // при необходимости добавьте другие значения по умолчанию
+    ...options,
+  };
+
+  if (options.expires instanceof Date) {
+    options.expires = options.expires.toUTCString();
+  }
+
+  let updatedCookie =
+    encodeURIComponent(name) + "=" + encodeURIComponent(value);
+
+  for (let optionKey in options) {
+    updatedCookie += "; " + optionKey;
+    let optionValue = options[optionKey];
+    if (optionValue !== true) {
+      updatedCookie += "=" + optionValue;
+    }
+  }
+
+  document.cookie = updatedCookie;
+}
+
+let num = document.querySelector(".value");
+let numValue = num.innerHTML;
+
+function up() {
+  let val = getCookie("value");
+  setCookie("value", +val + 1);
+  numValue = val;
+}
+
+function down() {
+  let val = getCookie("value");
+  setCookie("value", +val - 1);
+  numValue = val;
+}
+document.querySelector(".arrow_up").addEventListener("click", up);
+document.querySelector(".arrow_down").addEventListener("click", down);
