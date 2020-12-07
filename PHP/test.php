8318fc37aaec767959ce72c7d1185f65fc238e6d
samirtula <samirtula@mail.ru>
2020-12-07 21:23:06 +0300

    PHP

---
+++ PHP/test.php
@@ -0,0 +1,55 @@
+<?php
+
+/* $name = "Ivan";
+
+echo $name;
+$num = 1;
+$double = 1.2;
+$array1 = [1, 'test', 'test2'];
+print_r($array1);
+
+echo "<pre>";
+print_r($array1);
+echo "</pre>";
+
+
+$fakeNumber = "1";
+var_dump($fakeNumber);
+
+$bool = true;
+
+$sumString = "3" . "vs";
+$sumStr .= "32";
+
+var_dump((bool) $fakeNumber);
+
+
+class MyClass
+{
+};
+$obj = new MyClass();
+var_dump($obj);
+ */
+
+/* if(){
+    elseif(){
+
+    } else{}
+}
+ */
+$test2 = ($number > 3) ? 1 : 3;
+
+
+foreach ($array1 as $k => $val) {
+    echo $k;
+    echo "-";
+    echo $val;
+    echo "<br>";
+};
+
+for ($i; $i > 3; $i++) {
+};
+
+while ($i < 5) {
+    $i++;
+};
