4d27b4639d283cf5cd43416eb7db8195d0386586
samirtula <samirtula@mail.ru>
2020-12-09 21:18:28 +0300

    PHP

--- PHP/test.php
+++ PHP/test.php
@@ -37,7 +37,7 @@ var_dump($obj);
     } else{}
 }
  */
-$test2 = ($number > 3) ? 1 : 3;
+/* $test2 = ($number > 3) ? 1 : 3;
 
 
 foreach ($array1 as $k => $val) {
@@ -53,3 +53,66 @@ for ($i; $i > 3; $i++) {
 while ($i < 5) {
     $i++;
 };
+ */
+
+/* $first = 2;
+$second = 2;
+
+$result = $second % $first;
+
+
+if ($result === 0) {
+    echo "верно";
+}
+$y = 2019;
+$m = 3;
+$date= date('t', strtotime($y."-".$m));
+var_dump($date);
+?>
+ */
+
+/* 
+for ($i = 2; $i <= 10; $i++) {
+
+    for ($j = 2; $j <= 10; $j++) {
+        $num = $i * $j;
+        echo ($i . '*' . $j . "=" . $num . "<br>");
+    }
+};
+ */
+
+
+$percent = 0.2;
+$sum = 300;
+$years = 20;
+$arr = [];
+$percentSum = $sum * $percent;
+$sumWidthPercentSum = $sum + $percentSum;
+
+for ($i = 0; $i < $years; $i++) {
+
+    $percentSum = $sum * $percent;
+    $sumWithPercent = $sum + $percentSum;
+    $arr[] = [
+        'sum' => round($sum, 2),
+        'sumWithPercent' => round($sumWithPercent, 2),
+        'percentSum' => round($percentSum, 2)
+    ];
+    $sum = $sum + $percentSum;
+}
+
+?>
+
+<table>
+    <tbody>
+        <?php foreach ($arr as $k => $item) : ?>
+            <tr>
+                <td><?= $k + 1 ?></td>
+                <td><?= $item['sum'] ?></td>
+                <td><?= $item['sumWithPercent'] ?></td>
+                <td><?= $item['percentSum'] ?></td>
+            </tr>
+        <?php endforeach ?>
+    </tbody>
+
+</table>
