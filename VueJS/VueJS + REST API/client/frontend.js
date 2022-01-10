31d39106a2936245f6ef53fca0e3ec5b029be2cb
Samir <samirtula@mail.ru>
2022-01-11 02:13:12 +0300

    VueJS+REST

---
+++ VueJS/VueJS + REST API/client/frontend.js
@@ -0,0 +1,28 @@
+import Vue from 'https://cdn.jsdelivr.net/npm/vue@2.6.14/dist/vue.esm.browser.js';
+
+new Vue({
+    el: '#app',
+    data() {
+        return {
+            form: {
+                name: '',
+                value: ''
+            },
+            contacts: []
+        }
+  
+    },
+    methods: {
+        createContact() {
+            const { ...contact } = this.form;
+            this.contacts.push({...contact, id: Date.now()});
+            this.form.name = this.form.value = '';
+        },
+        markContact(id) {
+            console.log(id);
+        },
+        removeContact(id) {
+            
+        }
+    }
+})
