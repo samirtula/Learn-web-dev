0699927a12575240015d19813277f4cbfdacc2b8
Samir <samirtula@mail.ru>
2022-02-14 00:11:03 +0300

    react learning start

---
+++ React/react-theory/src/App.js
@@ -0,0 +1,36 @@
+import './App.css';
+import React from "react";
+
+
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img className="App-logo" alt="logo" />
+        <p>
+          OH Come on.Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+        <p> Hello</p>
+      </header>
+    </div>
+/*      React.createElement(
+          'div',
+          {className: 'App'},
+           React.createElement(
+          'h1',
+          null,
+          'Hello world'
+          ))*/
+  );
+}
+
+export default App;
